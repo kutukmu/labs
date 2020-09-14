0ad3face9770b8257fa6c2fccf4833fb228a7f6f
Kerim <kutukmu@gmail.com>
2020-09-13 19:02:47 -0500

    github

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,22 @@
 import React from "react"
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="footer-inner">
-                <div className="social-link"></div>
-                <div className="copy"><a rel="noreferrer" target="_blank" href="https://github.com/kutukmu/labs">Muhammed Kerim Kutuk &copy;</a></div>
-            </div>
+  return (
+    <div className="footer">
+      <div className="footer-inner">
+        <div className="social-link"></div>
+        <div className="copy">
+          <a
+            rel="noreferrer"
+            target="_blank"
+            href="https://github.com/kutukmu/labs"
+          >
+            Github Repository&copy;
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-
-export default Footer;
-
+export default Footer
